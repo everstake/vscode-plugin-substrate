fe80edfdc06485160f741bc46d0fea18ae8716ff
David Ackerman <enfipy@gmail.com>
2019-09-11 10:58:24 +0300

    Fix copy webpack error

--- src/commands/accounts/copyAddress.ts
+++ src/commands/accounts/copyAddress.ts
@@ -1,12 +1,12 @@
 import * as vscode from 'vscode';
-import * as cp from 'copy-paste';
+import copy from 'copy-to-clipboard';
 
 import BaseCommand from "@/common/baseCommand";
 import { AccountItem } from "@/trees";
 
 export class CopyAddressCommand extends BaseCommand {
     async run(item: AccountItem) {
-        cp.copy(item.description);
+        copy(item.description);
         vscode.window.showInformationMessage('Adddress copied to clipboard');
     }
 }
