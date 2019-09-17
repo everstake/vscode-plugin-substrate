68f71df3e4706f1f6c58b08f52a14c0afe323fbc
David Ackerman <enfipy@gmail.com>
2019-09-17 11:32:16 +0300

    Alter upload wasm

--- src/commands/contracts/uploadWasm.ts
+++ src/commands/contracts/uploadWasm.ts
@@ -1,42 +1,107 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import { KeyringPair$Json } from '@polkadot/keyring/types';
+import * as path from 'path';
+import { KeyringPair } from '@polkadot/keyring/types';
 import keyringUI from '@polkadot/ui-keyring';
-import { Keyring } from '@polkadot/keyring';
 import { compactAddLength } from '@polkadot/util';
 
 import BaseCommand from "@/common/baseCommand";
 import { MultiStepInput } from '@/common';
+import { AccountKey } from '@/substrate';
 
 type UploadWasmArgs = {
-    account: KeyringPair$Json,
-    code: string,
-    contract_name: string,
-    endowment: string,
+    account: KeyringPair,
+    compiled_contract: Uint8Array,
+    code_bundle_name: string,
+    contract_abi: string,
     max_gas: number,
 };
 
-interface LoadEvent {
-  target: {
-    result: ArrayBuffer;
-  };
-}
-
 export class UploadWasmCommand extends BaseCommand {
     options = {
-        title: 'Run extrinsic command',
-        totalSteps: 2,
+        title: 'Upload WASM command',
+        totalSteps: 5,
         ignoreFocusOut: true,
     };
 
     async run() {
         const state = {} as Partial<UploadWasmArgs>;
-        const argResult = await MultiStepInput.run(input => this.addAccount(input, state));
+        const argResult = await MultiStepInput.run(input => this.addCode(input, state));
         if (!argResult) {
             vscode.window.showInformationMessage('Extrinsic execution canceled');
             return;
         }
         const value = state as UploadWasmArgs;
+
+        const conn = this.substrate.getConnection();
+        if (!conn) {
+            throw Error('Not connected');
+        }
+        const contractApi = conn.tx.contracts ? conn.tx['contracts'] : conn.tx['contract'];
+        const res = await contractApi.putCode(value.max_gas, value.code_bundle_name).signAndSend(value.account);
+
+        // Todo: Subscribe for published code hash
+        console.log(res);
+    }
+
+    async addCode(input: MultiStepInput, state: Partial<UploadWasmArgs>) {
+        const uri = await input.showOpenDialog({
+            ...this.options,
+            shouldResume: async () => true,
+            step: input.CurrentStepNumber,
+            openLabel: 'Upload WASM',
+            canSelectFiles: true,
+            canSelectFolders: false,
+            canSelectMany: false,
+            filters: {
+                'WebAssembly': ['wasm'],
+            },
+        });
+        if (!uri || uri && uri.length <= 0) {
+            throw Error('Code not specified');
+        }
+        const codePath = uri[0].fsPath;
+        const wasm: Uint8Array = fs.readFileSync(codePath);
+        const isWasmValid = wasm.subarray(0, 4).toString() === '0,97,115,109'; // '\0asm'
+        if (isWasmValid) {
+            throw Error('Invalid code');
+        }
+        state.compiled_contract = compactAddLength(wasm);
+        const tmp = path.parse(codePath);
+        state.code_bundle_name = tmp.base;
+        return (input: MultiStepInput) => this.addCodeBundleName(input, state);
+    }
+
+    async addCodeBundleName(input: MultiStepInput, state: Partial<UploadWasmArgs>) {
+        state.code_bundle_name = await input.showInputBox({
+            ...this.options,
+            step: input.CurrentStepNumber,
+            prompt: 'A name for the WASM code to help users distinguish',
+            placeholder: 'ex. Flipper contract code',
+            ignoreFocusOut: true,
+            value: (typeof state.code_bundle_name === 'string') ? state.code_bundle_name : '',
+            validate: async (value) => (!value || !value.trim()) ? 'Code bundle name is required' : ''
+        });
+        return (input: MultiStepInput) => this.addMaxGas(input, state);
+    }
+
+    async addMaxGas(input: MultiStepInput, state: Partial<UploadWasmArgs>) {
+        state.max_gas = await input.showInputBox({
+            ...this.options,
+            step: input.CurrentStepNumber,
+            prompt: 'The maximum amount of gas that can be used by this deployment',
+            placeholder: 'ex. 10000',
+            ignoreFocusOut: true,
+            value: (typeof state.max_gas === 'string') ? state.max_gas : '',
+            validate: async (value) => {
+                if (!value || !value.trim()) {
+                    return 'Maximum gas is required';
+                }
+                // Todo: Check is it number
+                return '';
+            },
+        });
+        return (input: MultiStepInput) => this.addAccount(input, state);
     }
 
     async addAccount(input: MultiStepInput, state: Partial<UploadWasmArgs>) {
@@ -59,49 +124,30 @@ export class UploadWasmCommand extends BaseCommand {
         if (!account) {
             throw Error('Account not found');
         }
-        state.account = account;
-        return (input: MultiStepInput) => this.addCode(input, state);
+        return (input: MultiStepInput) => this.addPassword(input, state, account);
     }
 
-    async addCode(input: MultiStepInput, state: Partial<UploadWasmArgs>) {
-        const uri = await input.showOpenDialog({
+    async addPassword(input: MultiStepInput, state: Partial<UploadWasmArgs>, account: AccountKey) {
+        const keyring = this.substrate.getKeyring();
+        state.account = keyring.addFromJson(account);
+        await input.showInputBox({
             ...this.options,
-            shouldResume: async () => true,
             step: input.CurrentStepNumber,
-            openLabel: 'Upload WASM',
-            canSelectFiles: true,
-            canSelectFolders: false,
-            canSelectMany: false,
-            filters: {
-                'WebAssembly': ['wasm'],
+            prompt: 'Account password',
+            placeholder: 'ex. StrongPassword',
+            password: true,
+            value: '',
+            validate: async (pass) => {
+                try {
+                    state.account!.decodePkcs8(pass);
+                    if (state.account!.isLocked) {
+                        return 'Failed to decode account';
+                    }
+                    return '';
+                } catch (err) {
+                    return `Failed to decode account: ${err.message}`;
+                }
             },
         });
-        if (!uri || uri && uri.length <= 0) {
-            throw Error('Code not specified');
-        }
-        const codePath = uri[0].path;
-
-        const wasm: Uint8Array = fs.readFileSync(codePath);
-        const isWasmValid = wasm.subarray(0, 4).toString() === '0,97,115,109'; // '\0asm'
-        if (isWasmValid) {
-            throw Error('Invalid code');
-        }
-        const result = compactAddLength(wasm);
-
-        const conn = this.substrate.getConnection();
-        if (!conn) {
-            throw Error('Not connected');
-        }
-        const contractApi = conn.tx.contracts ? conn.tx['contracts'] : conn.tx['contract'];
-        // Todo: Get decoded account
-        const keyring = new Keyring({ type: 'sr25519' });
-        const acc = keyring.addFromJson(state.account!);
-        acc.decodePkcs8('');
-        const res = await contractApi.putCode('10000000', result).signAndSend(acc);
-        // Todo: Subscribe for published code hash
-        console.log(res);
-
-        // keyring.saveContract()
-        // return (input: MultiStepInput) => this.addPassword(input, state, account);
     }
 }
